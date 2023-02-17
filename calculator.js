23f3b94a44ef43079583ff3bd8cb2a000b6cd5d7
kmert0@outlook.com <kmert0@outlook.com>
2023-02-17 23:20:02 +0300

    bmiCalculator

--- calculator.js
+++ calculator.js
@@ -16,6 +16,17 @@ app.post("/", function (req, res) {
   res.send("Result is: " + result);
 });
 
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/" + "bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function (req, res) {
+  var height = parseFloat(req.body.Height);
+  var weight = parseFloat(req.body.Weight);
+  var bmi = weight / (height * height);
+  res.send("Your BMI is: " + bmi);
+});
+
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
